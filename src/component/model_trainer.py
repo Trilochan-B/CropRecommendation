a8d53c15a6bad1dd779a318ef3b04674ec33bba7
Trilochan-B <trilochantrilo600@gmail.com>
2025-01-14 08:26:04 +0530

    Project

---
+++ src/component/model_trainer.py
@@ -0,0 +1,79 @@
+import sys
+import os
+
+from src.logger import logging
+from src.exception import CustonmException
+from src.utill import save_object,evaluate_models
+
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor
+
+from dataclasses import dataclass
+
+@dataclass
+class modelTrainerConfig:
+    trained_model_path = os.path.join("artifacts","model.pkl")
+
+class modelTrainer:
+    def __init__(self):
+        self.model_train_config = modelTrainerConfig()
+
+    def initiate_model_trainer(self,train_arr,test_arr):
+        try:
+            x_train, y_train, x_test, y_test = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+            
+            models ={
+                "LinearRegressor" : LinearRegression(),
+                "DecisionTreee": DecisionTreeRegressor(),
+                "KNeighbour" : KNeighborsRegressor(),
+                "RandomForest": RandomForestRegressor(),
+                "AdaBoostRegressor" : AdaBoostRegressor(),
+                "GradientBoostingRegressor" : GradientBoostingRegressor()
+            }
+
+            params = {
+                "LinearRegressor" : {},
+                "DecisionTreee" : {
+                    'criterion' : ['squared_error','friedman_mse','absolute_error','poisson']
+                },
+                "KNeighbour" : {
+                    'n_neighbors' : [3,5,7,9,11]
+                },
+                "RandomForest" : {
+                    'n_estimators' : [8,16,32,64,128,256]
+                },
+                "AdaBoostRegressor" : {
+                    'learning_rate' : [.1,.01,0.05,0.001],
+                    'n_estimators' : [8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor" : {
+                    'subsample' : [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators' : [8,16,32,64,128,256],
+                    'learning_rate' : [.1,.01,0.05,0.001]
+                }
+
+            }
+
+            logging.info("model training initiated")
+
+            model_report:dict = evaluate_models(x_train,y_train,x_test,y_test,models,params)
+
+
+            best_model_score = max(sorted(model_report.values()))
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            if best_model_score < 0.6:
+                raise CustonmException("No best model found")
+            logging.info(f"Best model :{best_model_name} score :{best_model_score}")
+
+            best_model = models[best_model_name]
+            save_object(obj=best_model, file_path=self.model_train_config.trained_model_path)
+        except Exception as e:
+            raise CustonmException(e,sys)
---
+++ src/utill.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from src.exception import CustonmException
+from src.logger import logging
+import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+def save_object(obj, file_path):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok= True)
+        with open(file_path,"wb") as file :
+            dill.dump(obj, file)
+    except Exception as e:
+        raise CustonmException(e,sys)
+    
+def evaluate_models(x_train,y_train,x_test,y_test,models,params):
+    try:
+        report ={}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = list(params.values())[i]
+            
+            gs = GridSearchCV(estimator=model,param_grid=param)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            y_pred = model.predict(x_test)
+
+            score = r2_score(y_true=y_test,y_pred=y_pred)
+
+            report[list(models.keys())[i]] = score
+
+        return report
+
+
+
+    except Exception as e:
+        raise CustonmException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as f:
+            return dill.load(f)
+    except Exception as e:
+        raise CustonmException(e,sys)
